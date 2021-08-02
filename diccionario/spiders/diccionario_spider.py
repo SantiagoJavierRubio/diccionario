7866afcbab9538d5a654cec3c57d5657c4c47745
Santiago Rubio <s.rubio91@gmail.com>
2021-08-02 09:39:24 -0300

    diccionario

---
+++ diccionario/spiders/diccionario_spider.py
@@ -0,0 +1,34 @@
+# import scrapy
+# import unidecode
+# from diccionario.items import DiccionarioItem
+
+# class DiccionarioSpider(scrapy.Spider):
+#     name = 'diccionario'
+#     start_urls = ['http://www.culturageneral.net/palabrastecnicas/']
+
+#     def parse(self, response):
+#         palabras = response.xpath('//b/text()').getall()
+#         definiciones = response.xpath('//td/text()').getall()
+
+#         def_list = []
+
+#         for definicion in definiciones:
+#             text = ' '.join(definicion.split())
+#             if text == '':
+#                 continue
+#             else:
+#                 def_list.append(text)
+
+#         for palabra in palabras:
+#             if palabra == 'Palabra' or palabra == 'Definición':
+#                 continue
+#             else:
+#                 palabra = palabra.replace(' ', '')
+#                 palabra = unidecode.unidecode(palabra)
+#                 definicion_valida = def_list.pop(0)
+
+#                 item = DiccionarioItem()
+#                 item['palabra'] = palabra
+#                 item['definicion'] = definicion_valida
+
+#                 yield item
